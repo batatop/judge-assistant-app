e8f7e75959d0eb89865982a8c25df1351b4d5b06
batatop <batatop@gmail.com>
2024-01-06 21:25:21 +0300

    added logout and navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import { auth } from '../../firebase'; 
+import { signOut } from 'firebase/auth';
+import './Navbar.css'
 
 export default class Navbar extends Component {
+  logout = () => {
+    signOut(auth).then(() => {
+      console.log('User signed out');
+    }).catch((error) => {
+      console.error('Error signing out: ', error);
+    });
+  };
+
   render() {
     return (
-      <div>Navbar</div>
+      <div className='navbarContainer'>
+        <div>JAI</div>
+        <div>
+          <div className='navbarButton' onClick={this.logout}>Logout</div>
+        </div>
+      </div>
     )
   }
 }
