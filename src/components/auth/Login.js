2f3b0f0a16806fc458cfd53c7cb807178b6f6de4
batatop <batatop@gmail.com>
2024-01-07 15:49:27 +0300

    updated login design

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { auth } from '../../firebase'; // assuming you have a firebase.js file
 import AppInput from '../general/AppInput';
 import AppButton from '../general/AppButton';
+import './Auth.css';
 
 export default class Login extends Component {
   constructor(props) {
@@ -28,10 +29,13 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div>
-        <AppInput value={this.state.email} onChange={this.handleChange} type="email" name="email" placeholder="Enter email" />
-        <AppInput value={this.state.password} onChange={this.handleChange} type="password" name="password" placeholder="Enter password" />
-        <AppButton value="Login" onClick={this.login} />
+      <div className='authContainer'>
+        <div className='authInnerContainer'>
+          <div className='appAuthTitle'>JAI</div>
+          <AppInput value={this.state.email} onChange={this.handleChange} type="email" name="email" placeholder="Enter email" />
+          <AppInput value={this.state.password} onChange={this.handleChange} type="password" name="password" placeholder="Enter password" />
+          <AppButton value="Login" onClick={this.login} />
+        </div>
       </div>
     )
   }
